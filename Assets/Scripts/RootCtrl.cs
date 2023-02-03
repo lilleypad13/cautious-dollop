3fd76568f7dea04df01c2f028c2e9631e27cce87
Kylie <kr.gilde@gmail.com>
2023-02-03 01:51:14 -0500

    Roots can split into branches every given number of segments. Segment rotation is now based on the rotation of the parent segment.

--- Assets/Scripts/RootCtrl.cs
+++ Assets/Scripts/RootCtrl.cs
@@ -7,13 +7,14 @@ public class RootCtrl : MonoBehaviour
 {
 	public float segmentLength;
 	public float growthSpeed;
-	public float branchChance;
+	public Vector2Int segmentsBetweenBranches;
 	[Range(0f, 90f)] public float angleVariance;
 	[Range(0f, 90f)] public float branchAngleVariance;
 	public RootSegment segmentPrefab;
 
 	private List<RootSegment> rootList = new List<RootSegment>();
 	private List<RootSegment> tempList = new List<RootSegment>();
+	private int segmentsSinceBranch = 0;
 
 	private void Start() {
 		rootList.Add(CreateSegment(transform.position));
@@ -24,7 +25,17 @@ public class RootCtrl : MonoBehaviour
 			root.Length += growthSpeed * Time.deltaTime;
 			if(root.Length >= segmentLength) {
 				root.EnableGrowth(false);
-				tempList.Add(CreateSegment(root.tip.position));
+				if(root.transform.parent.childCount >= segmentsBetweenBranches.x) {
+					float chance = 1 / (segmentsBetweenBranches.y - segmentsBetweenBranches.x + 1.0f);
+					Debug.Log($"{root.transform.parent.childCount} since branch, current chance {chance * (root.transform.parent.childCount - segmentsBetweenBranches.x + 1.0f)}");
+					if (Random.Range(0f, 1f) <= chance * (root.transform.parent.childCount - segmentsBetweenBranches.x + 1.0f)) {
+						CreateBranches(root, tempList);
+					} else {
+						tempList.Add(CreateSegment(root));
+					}
+				} else {
+					tempList.Add(CreateSegment(root));
+				}
 			}
 		}
 		rootList = rootList.Concat(tempList).Where(x => x.IsGrowing).ToList();
@@ -32,14 +43,29 @@ public class RootCtrl : MonoBehaviour
 	}
 
 	private RootSegment CreateSegment(Vector3 startPos) {
-		Quaternion rot = Quaternion.AngleAxis(Random.Range(-angleVariance, angleVariance), Vector3.forward);
-		RootSegment newSegment = Instantiate(segmentPrefab, startPos, rot, transform);
-		return newSegment;
+		return Instantiate(segmentPrefab, startPos, Quaternion.AngleAxis(Random.Range(-angleVariance, angleVariance), Vector3.forward), BranchParent());
 	}
 
 	private RootSegment CreateSegment(RootSegment parent) {
-		Quaternion rot = Quaternion.AngleAxis(Random.Range(-angleVariance, angleVariance), parent.transform.right);
-		RootSegment newSegment = Instantiate(segmentPrefab, parent.tip.position, rot, transform);
-		return newSegment;
+		Vector3 angle = Quaternion.AngleAxis(Random.Range(-angleVariance, angleVariance), parent.transform.forward).eulerAngles + parent.transform.eulerAngles;
+		return Instantiate(segmentPrefab, parent.tip.position, Quaternion.Euler(angle), parent.transform.parent);
+	}
+
+	private void CreateBranches(RootSegment parent, List<RootSegment> addList) {
+		float angle = Random.Range(0f, branchAngleVariance);
+		Debug.Log($"Creating branches at angle {angle}");
+		addList.Add(Instantiate(segmentPrefab, parent.tip.position, 
+			Quaternion.Euler(Quaternion.AngleAxis(angle, parent.transform.forward).eulerAngles + parent.transform.eulerAngles), 
+			BranchParent()));
+		addList.Add(Instantiate(segmentPrefab, parent.tip.position, 
+			Quaternion.Euler(Quaternion.AngleAxis(-angle, parent.transform.forward).eulerAngles + parent.transform.eulerAngles), 
+			BranchParent()));
+	}
+
+	private Transform BranchParent() {
+		GameObject g = new GameObject("Branch");
+		g.transform.parent = transform;
+		g.transform.localPosition = Vector3.zero;
+		return g.transform;
 	}
 }
